de00cc23a202921c819422c78f68be54f25cff58
kntaka <knt.takahashii@gmail.com>
2020-06-06 23:58:16 +0900

    Add handler

---
+++ handler.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
+)
+
+type interactionHandler struct {
+	slackClient       *slack.Client
+	verificationToken string
+}
+
+func (h interactionHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		log.Printf("[ERROR] Invalid method: %s", r.Method)
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	buf := new(bytes.Buffer)
+	_, _ = buf.ReadFrom(r.Body)
+	body := buf.String()
+
+	eventsAPIEvent, err := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionNoVerifyToken())
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	switch eventsAPIEvent.Type {
+	case slackevents.URLVerification:
+		var r *slackevents.ChallengeResponse
+		err := json.Unmarshal([]byte(body), &r)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		w.Header().Set("Content-Type", "text")
+		w.Write([]byte(r.Challenge))
+	case slackevents.CallbackEvent:
+		innerEvent := eventsAPIEvent.InnerEvent
+		switch ev := innerEvent.Data.(type) {
+		case *slackevents.AppMentionEvent:
+			_, _, _ = h.slackClient.PostMessage(ev.Channel, slack.MsgOptionText("Yes, hello.", false))
+		}
+	default:
+		log.Printf("[ERROR] ]Invalid event type was submitted: %s", eventsAPIEvent.Type)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
