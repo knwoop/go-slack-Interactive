8c6ac03161e320f395381d54b1041455d7c76b24
kntaka <knt.takahashii@gmail.com>
2020-06-06 23:58:35 +0900

    Add handler to main

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/knwoop/go-slack-Interactive
 
 go 1.14
 
-require github.com/slack-go/slack v0.6.5
+require (
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/slack-go/slack v0.6.5
+)
--- main.go
+++ main.go
@@ -1,69 +1,47 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"strings"
 
+	"github.com/kelseyhightower/envconfig"
 	"github.com/slack-go/slack"
-	"github.com/slack-go/slack/slackevents"
 )
 
+type Env struct {
+	// Port is port to listen. Default is 5000.
+	Port string `envconfig:"PORT" default:"5000"`
+
+	// BotToken is bot user token to access to slack API.
+	SlackBotToken string `envconfig:"SLACK_BOT_TOKEN" required:"true"`
+
+	// VerificationToken is used to validate interactive messages from slack.
+	VerificationToken string `envconfig:"VERIFICATION_TOKEN" required:"true"`
+
+	// BotID is bot user ID.
+	BotID string `envconfig:"BOT_ID" required:"true"`
+
+	// ChannelID is slack channel ID where bot is working.
+	// Bot responses to the mention in this channel.
+	ChannelID string `envconfig:"CHANNEL_ID" required:"true"`
+}
+
 func main() {
-	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
-	http.HandleFunc("/slack/events", func(w http.ResponseWriter, r *http.Request) {
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Println(err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
+	os.Exit(run())
+}
 
-		eventsAPIEvent, err := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionNoVerifyToken())
-		if err != nil {
-			log.Println(err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
+func run() int {
+	var env Env
+	if err := envconfig.Process("", &env); err != nil {
+		log.Printf("[ERROR] Failed to process env var: %s", err)
+		return 1
+	}
+	api := slack.New(env.SlackBotToken)
 
-		switch eventsAPIEvent.Type {
-		case slackevents.URLVerification:
-			var res *slackevents.ChallengeResponse
-			if err := json.Unmarshal(body, &res); err != nil {
-				log.Println(err)
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			w.Header().Set("Content-Type", "text/plain")
-			if _, err := w.Write([]byte(res.Challenge)); err != nil {
-				log.Println(err)
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-		case slackevents.CallbackEvent:
-			innerEvent := eventsAPIEvent.InnerEvent
-			switch event := innerEvent.Data.(type) {
-			case *slackevents.AppMentionEvent:
-				message := strings.Split(event.Text, " ")
-				if len(message) < 2 {
-					w.WriteHeader(http.StatusBadRequest)
-					return
-				}
-
-				command := message[1]
-				switch command {
-				case "ping":
-					if _, _, err := api.PostMessage(event.Channel, slack.MsgOptionText("pong", false)); err != nil {
-						log.Println(err)
-						w.WriteHeader(http.StatusInternalServerError)
-						return
-					}
-				}
-			}
-		}
+	http.Handle("/interaction", interactionHandler{
+		slackClient:       api,
+		verificationToken: env.VerificationToken,
 	})
 
 	log.Println("[INFO] Server listening")
